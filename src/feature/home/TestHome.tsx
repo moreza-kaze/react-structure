094ed92ffdfccb96657636845b3e9d4042c6196b
moreza-kaze <morezakaze380@gmail.com>
2023-02-13 12:19:57 +0330

    add feature

---
+++ src/feature/home/TestHome.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Test : React.FC = () => {
+    return(
+        <>
+        <h1>home/test</h1>
+        </>
+    )
+}
+
+export  default Test
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Button from '../components/Button'
+import Test from '../feature/home/TestHome'
 
 function App() {
   return (
-    <div className="flex bg-black justify-center text-3xl font-bold underline ">
+    <>
+      <div className="flex bg-black justify-center text-3xl font-bold underline ">
       <h1 className='text-red-500'>hello world</h1>
       <Button className='text-white'>eweer</Button>
-    </div>
+      </div>
+      <div>
+        <Test />
+      </div>
+    </>
   )
 }
 
