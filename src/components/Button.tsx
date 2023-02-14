78331ebb11b3eace7b2f38ae49e822319fddf95d
moreza-kaze <morezakaze380@gmail.com>
2023-02-14 13:19:16 +0330

    add on click to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,22 +2,27 @@ import * as React from 'react';
 import Ripple from 'material-ripple-effects';
 
 interface ButtonInterface {
-  className?: string;
-  children: string;
+   className?: string;
+   onClick?: React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>;
+   children: string;
 }
 
 const Button: React.FC<ButtonInterface> = ({
-  className,
-  children
+   className,
+   onClick,
+   children
 }: ButtonInterface) => {
-  const ripple = new Ripple();
-  return (
-    <button
-      className={`h-10 w-23 border border-solid border-gray-300 ${className}`}
-      onMouseDown={(e) => ripple.create(e, 'light')}
-    >
-      {children}
-    </button>
-  );
+   const ripple = new Ripple();
+   return (
+      <button
+         className={`h-10 w-23 border border-solid border-gray-300 ${className}`}
+         onMouseDown={(e) => ripple.create(e, 'light')}
+         onClick={(e) => {
+            if (onClick) onClick(e);
+         }}
+      >
+         {children}
+      </button>
+   );
 };
 export default Button;
